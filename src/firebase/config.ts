447025e0db42659166fa8b1d31362eb485c09343
Rajan Prasaila Yadav <rajanprasaila@gmail.com>
2025-11-28 17:54:08 +0000

    it is already there localhost

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@
 export const firebaseConfig = {
   apiKey: 'AIzaSyAGAdn6hT9fhaGqmqN5LpcUlWV3FW-rNTw',
   authDomain: 'studio-2526433000-2e931.firebaseapp.com',
-  projectId: 'studio-2526433000-2e9f3',
+  projectId: 'studio-2526433000-2e931',
   storageBucket: 'studio-2526433000-2e931.appspot.com',
   messagingSenderId: '548223774566',
   appId: '1:548223774566:web:16693942e42061dd6a22a7',
