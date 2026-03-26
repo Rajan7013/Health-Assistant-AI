0e959ebad5bebf6a1ca7090a7927246b4b10ac93
Rajan7013 <rajanprasaila@gmail.com>
2026-03-27 00:34:27 +0530

    Delete mobile-app directory
    unwanted files and secreate variables expose

--- mobile-app/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-import tw from 'twrnc';
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
-export { tw };
